82f1aa256e5f1a0c96165b8d82941397bec98ec2
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2024-09-04 18:57:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Pagination from "react-bootstrap/Pagination";
+import { Row } from "react-bootstrap";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  console.log("currentPage", currentPage);
+  const [productsPerPage] = useState(4);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(products.length / productsPerPage);
+  // Get the current products for the page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // Handle next and prev page navigation
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Create the pagination items
+  const pageNumbers = []; //[1,2,3,4,5]
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  // Handle page change
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <Container>
+      <Row>
+        {currentProducts.map((product) => (
+          <Col style={{ marginBottom: "20px" }} md={4} lg={3} key={product.id}>
+            <Card className="h-100 d-flex flex-column">
+              <Card.Img
+                style={{ height: "300px" }}
+                variant="top"
+                src={product.image}
+                alt={product.title}
+              />
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{product.title.substring(0, 10)}</Card.Title>
+                <Card.Text>{product.description.substring(0, 10)}...</Card.Text>
+                <Button variant="primary" className="mt-auto">
+                  Go somewhere
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      {/* Pagination Controls */}
+      <Pagination className="justify-content-center">
+        <Pagination.Prev
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        />
+        {pageNumbers.map((number) => (
+          <Pagination.Item
+            key={number}
+            active={number === currentPage}
+            onClick={() => paginate(number)}
+          >
+            {number}
+          </Pagination.Item>
+        ))}
+        <Pagination.Next
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        />
+      </Pagination>
+    </Container>
+  );
+};
+
+export default App;
